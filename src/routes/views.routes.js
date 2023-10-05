186588517cad841068b92ca821378880458d82b3
evealvarenga <ealvarenga095@gmail.com>
2023-10-04 22:11:54 -0300

    Update views.routes.js

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -19,7 +19,7 @@ router.get("/realtimeproducts", async (req, res) => {
         const products = await manager.getProducts(req.query);
         res.render("realTimeProducts", {response: products});
     } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al obtener los productos de los productos.' });
+        res.status(500).json({ message: error.message });
     }
 });
 
